3b8d51985715e134e28775c67aa1cf0c1996c331
RITUriya <rituriya093@gmail.com>
2021-07-20 11:10:08 +0530

    third commit

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,23 +1,22 @@
 import { AuthServiceService } from './auth-service.service';
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { SocialUser } from 'angularx-social-login';
-import { Router } from '@angular/router';
-import 'rxjs/add/operator/map';
+import { CanActivate, Router } from '@angular/router';
+import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  // user!: SocialUser;
   constructor(private auth: AuthServiceService, private router: Router) {}
 
-  CanActivate() {
-    this.auth.user.map((user) => {
-      if (user) return true;
-
-      this.router.navigate(['./login']);
-      return false;
-    });
+  canActivate() {
+    return this.auth.user$.pipe(
+      map((user) => {
+        if (user) return true;
+        this.router.navigate(['./login']);
+        return false;
+      })
+    );
   }
 }
--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { SocialAuthService, SocialUser } from 'angularx-social-login';
 import { GoogleLoginProvider } from 'angularx-social-login';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServiceService {
-  public user!: SocialUser;
+  //public user!: SocialUser;
+  //public isLoggedIn: Subscription;
+  user$: Observable<SocialUser>;
+  public userName!: SocialUser;
   constructor(private socialAuthService: SocialAuthService) {
-    socialAuthService.authState.subscribe((user) => (this.user = user));
+    //socialAuthService.authState.subscribe((user) => (this.user = user));
+    this.user$ = socialAuthService.authState;
+    socialAuthService.authState.subscribe(
+      (userName) => (this.userName = userName)
+    );
   }
 
   onClickLoginWithGoogle(): void {
